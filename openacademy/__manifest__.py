9ea4f237098f58a372750a15a527c1d3e214fedf
joseagrandal <josea.grandal@gmail.com>
2018-04-13 05:12:03 +0200

    [REF] openacademy:Add security

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,11 +25,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        
     ],
     # only loaded in demonstration mode
     'demo': [
