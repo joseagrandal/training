e5d17f9c1b432b0e136ffd50b5ed4ba0e1173f83
joseagrandal <josea.grandal@gmail.com>
2018-04-13 02:49:40 +0200

    [REF] advanced views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 class Course(models.Model):
@@ -41,16 +42,18 @@ class Session(models.Model):
 
 	name = fields.Char(required=True)
 	start_date = fields.Date(default=fields.Date.today)
-	start_date_time = fields.Datetime(default=fields.Datetime.now)
 	duration = fields.Float(digits=(6,2), help="Duration in days")
+	end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+	hours = fields.Float(string="Duration in hours", compute='_get_hours', inverse='_set_hours')
 	active = fields.Boolean(default=True)
 	seats = fields.Integer(string="Number of seats")
 	taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-	
 	instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|',('instructor','=','True'),('category_id.name','ilike','Teacher')])
 	course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
+	attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
+	color = fields.Integer()
+	
 	@api.depends('seats','attendee_ids')
 	def _taken_seats(self):
 		# import pdb; pdb.set_trace()
@@ -59,6 +62,11 @@ class Session(models.Model):
 				r.taken_seats = 0.0
 			else:
 				r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+	@api.depends('attendee_ids')
+	def _get_attendees_count(self):
+		for r in self:
+			r.attendees_count = len(r.attendee_ids)
 	
 	@api.onchange('seats', 'attendee_ids')
 	def _verify_valid_seats(self):
@@ -84,3 +92,28 @@ class Session(models.Model):
 			if r.instructor_id in r.attendee_ids:
 				raise exceptions.ValidationError("A session's instructor can't be an attendee")
 
+	@api.depends('start_date', 'duration')
+	def _get_end_date(self):
+		for r in self:
+			if not (r.start_date and r.duration):
+				r.end_date = r.start_date
+				continue
+			start = fields.Datetime.from_string(r.start_date)
+			duration = timedelta(days=r.duration, seconds=-1)
+			r.end_date = start + duration
+	def _set_end_date(self):
+		for r in self:
+			if not (r.start_date and r.end_date):
+				continue
+			start_date = fields.Datetime.from_string(r.start_date)
+			end_date = fields.Datetime.from_string(r.end_date)
+			r.duration = (end_date - start_date).days + 1
+
+	@api.depends('duration')
+	def _get_hours(self):
+		for r in self:
+			r.hours = r.duration * 24
+	def _set_hours(self):
+		for r in self:
+			r.duration = r.hours / 24
+			
